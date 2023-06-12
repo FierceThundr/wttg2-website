a3bdc89dd1305e523b9b7b465a22acb8303c8fec
FierceThundr <51411635+FierceThundr@users.noreply.github.com>
2023-06-12 12:54:40 -0400

    Tweaks
    Modified shuffle for compatibility
    Undid additional state

--- Websites/12 Recreations Claffis/script.js
+++ Websites/12 Recreations Claffis/script.js
@@ -16,14 +16,12 @@ function note_handler(){
 	switch(data.state){
 		case 0: //If state is: Disabled
 			break;
-		case 1: //If state is: Initial
+		case 1: //If state is: Idle
 			if (note != 0) {
 				sound_note(note)
 				note_highlight(note)
 				break
 			}
-		case 2: //If state is: Idle
-			if (note != 0) {break}
 			data.state = 0
 			data.pattern = shuffle(Array.from({length:data.length},(_,i)=>i+1)).slice(0,data.length)
 			var play = [...data.pattern]
@@ -32,12 +30,12 @@ function note_handler(){
 				sound_note(next)
 				note_highlight(next)
 				if (play.length == 0) {
-					data.state = 3
+					data.state = 2
 					clearInterval(interval)
 				}
 			},2000)
 			break
-		case 3: //If state is: Guessing
+		case 2: //If state is: Guessing
 			if (note == 0) {break}
 			var next = data.pattern.shift()
 			if (note == next) {
@@ -52,7 +50,7 @@ function note_handler(){
 				}
 				break
 			}
-			data.state = 2
+			data.state = 1
 			sound_failure()
 			break
 	}
@@ -71,7 +69,7 @@ function note_highlight(index){
 	},500)
 }
 
-function shuffle(f){for(let l=f.length-1;l>0;l--){let o=Math.floor(Math.random()*(l+1));[f[l],f[o]]=[f[o],f[l]]}return f}
+function shuffle(f){for(let l=f.length-1;l>0;l--){let o=Math.floor(Math.random()*(l+1));var a=f[l];f[l]=f[o];f[o]=a}return f}
 
 /* Hook functions */
 
